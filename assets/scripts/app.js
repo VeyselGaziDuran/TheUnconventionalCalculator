523658af2204b09179c1493169887d0ac2a58c9a
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-06-25 14:15:13 +0300

    functions added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,10 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1; 
+function add(){
+    currentResult = currentResult + userInput.value;
+}
+  
+addBtn.addEventListener('click', add);
 
-outputResult(currentResult, '');
+outputResult(currentResult, calculationDescription); 
