cd88c28995df27c7d9fea55cfa838be284ce000e
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-07-06 19:27:56 +0300

    Update getUserNumberInput function to fix input retrieval issue
    Modified the getUserNumberInput function in app.js to resolve the error caused by not being able to retrieve the input value. Replaced 'usrInput' with 'input-number' as the correct ID for the input element in the HTML file. Now the function retrieves the user input correctly, allowing the calculator functions to work properly.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,49 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(){
-    currentResult = currentResult + userInput.value; 
-    outputResult(currentResult, '');
+function getUserNumberInput() {
+    const usrInput = document.getElementById('input-number');
+    return parseInt(usrInput.value);
 }
-  
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) { 
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription)
+ }
+
+function add(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber; 
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function substrack(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber; 
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+
+function multiply(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber; 
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber; 
+    createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', substrack);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
 
